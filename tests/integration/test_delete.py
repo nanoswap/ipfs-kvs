547dd501992f02d2838b8b00c378aea4a9a748d4
Nate Schultz <nate.schultz@outlook.com>
2023-05-14 13:21:47 -0400

    fixed test linting

---
+++ tests/integration/test_delete.py
@@ -0,0 +1,7 @@
+"""Tests for the Store.delete method."""
+import unittest
+
+
+class TestDelete(unittest.TestCase):
+    """Test class for Delete functionality."""
+    pass
--- tests/unit/test_delete.py
+++ tests/unit/test_delete.py
@@ -1,65 +1,57 @@
-import os
+"""Test the Store.delete functions."""
 import unittest
-from unittest.mock import patch, MagicMock
 import uuid
+from typing import Self
+from unittest.mock import patch
 
 from ipfsclient.ipfs import Ipfs
+
 from ipfskvs.index import Index
 from ipfskvs.store import Store
 
-class TestDelete(unittest.TestCase):
-    ipfs: Ipfs
 
-    def setUp(self) -> None:
+class TestDelete(unittest.TestCase):
+    """Test class for Delete functionality."""
+
+    def setUp(self: Self) -> None:
+        """Set up the test."""
         self.ipfs = Ipfs()
 
-    @patch.object(Ipfs, "delete")
-    @patch.object(Ipfs, "list_files")
     @patch("os.path.dirname")
-    def test_delete(self, mock_dirname, mock_list_files, mock_delete):
+    @patch.object(Ipfs, "list_files")
+    @patch.object(Ipfs, "delete")
+    def test_delete(
+            self: Self,
+            mock_delete: unittest.mock,
+            mock_list_files: unittest.mock,
+            mock_dirname: unittest.mock) -> None:
         """Test the delete function."""
-        
-        # Mock the os.path.dirname function to return a test value
         mock_dirname.side_effect = ["/test_dir/test_sub_dir", "/test_dir", "/"]
-
-        # Mock the Store instance
         store = Store(index=Index(index={
             "testindex1": str(uuid.uuid4()),
             "testindex2": str(uuid.uuid4())
         }), ipfs=self.ipfs)
-
-        # Mock the list_files function to return an empty list
         mock_list_files.return_value = []
-
-        # Call the delete function with check_directory set to True
         store.delete(check_directory=True)
-
-        # Assert that delete and list_files were called with the correct arguments
         mock_delete.assert_any_call("/test_dir/test_sub_dir")
         mock_delete.assert_any_call("/test_dir")
         mock_list_files.assert_called_with("/test_dir")
 
-    @patch.object(Ipfs, "delete")
-    @patch.object(Ipfs, "list_files")
     @patch("os.path.dirname")
-    def test__delete_if_empty(self, mock_dirname, mock_list_files, mock_delete):
+    @patch.object(Ipfs, "list_files")
+    @patch.object(Ipfs, "delete")
+    def test__delete_if_empty(
+            self: Self,
+            mock_delete: unittest.mock,
+            mock_list_files: unittest.mock,
+            mock_dirname: unittest.mock) -> None:
         """Test the _delete_if_empty function."""
-
-        # Mock the os.path.dirname function to return a test value
         mock_dirname.side_effect = ["/test_dir/test_sub_dir", "/test_dir", "/"]
-
-        # Mock the Store instance
         store = Store(index=Index(index={
             "testindex1": str(uuid.uuid4()),
             "testindex2": str(uuid.uuid4())
         }), ipfs=self.ipfs)
-
-        # Mock the list_files function to return an empty list
         mock_list_files.return_value = []
-
-        # Call the _delete_if_empty function
         store._delete_if_empty("test_dir")
-
-        # Assert that delete and list_files were called with the correct arguments
         mock_delete.assert_called_with("/test_dir")
         mock_list_files.assert_called_with("/test_dir")
